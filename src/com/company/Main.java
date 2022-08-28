f14f95f15bde82bdda0c31ae7db501f69f4e6caa
KozyaSkyworker <d_koz@inbox.ru>
2022-08-28 12:50:58 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,69 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+
+//Подсчитать среднее количество частоты встречающихся символов (вывести в консоль). Определить символ,
+// у которого частота будет наиболее приближена к полученному среднему значению
+// (вывести в консоль сам символ и значение кода в UTF-8)
+
+public class Main {
+
+    public static void  urlRequest (String stringURL) throws IOException{
+        URL url = new URL(stringURL);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null){
+            content.append(inputLine);
+        }
+
+        in.close();
+
+        con.disconnect();
+
+        charactersFrequency(content);
+    }
+
+    public static void charactersFrequency(StringBuilder content){
+        System.out.println("\nОтвет: " + content);
+
+        Map<Character,Integer> frequencies = new HashMap<>();
+        for (char ch : content.toString().toCharArray())
+            frequencies.put(ch, frequencies.getOrDefault(ch, 0) + 1);
+
+        int freqValueCount = 0;
+        System.out.println("\nЧастоты: ");
+        for (Map.Entry<Character,Integer> entry : frequencies.entrySet()){
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+            freqValueCount += entry.getValue();
+        }
+
+        double freqArithmeticMean = (double) freqValueCount / frequencies.size();
+
+        System.out.println("\nСреднее значение частот: " + freqArithmeticMean);
+        System.out.println("\nСимволы, которые соответствуют условию наиболее близкого значения частоты к среднему значанию: ");
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        int randomNum = ThreadLocalRandom.current().nextInt(0, 1000);
+
+        String url = "http://numbersapi.com/" + randomNum + "/trivia" ;
+
+        urlRequest(url);
+    }
+}
